317c0d22789a34bd6df57b82906bef593df5787f
mateher624 <mateher624@student.polsl.pl>
2016-04-19 21:50:18 +0200

    form fixes

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -8,15 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-public class Osoba
-{
-    public string name;
-    public string surname;
-    public string parents;
-    public DateTime date;
-    public string gender;
-    public string pesel;
-}
+
 
 
 
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Osoba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Osoba
+    {
+        public string name;
+        public string surname;
+        public string parents;
+        public DateTime date;
+        public string gender;
+        public string pesel;
+    }
+}
